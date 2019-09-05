fad00c362c8090121e3750767e729e184bb31531
PhieuDieu96 <PhieuDieu96@users.noreply.github.com>
2019-09-05 23:52:57 +0700

    Update bai4.js

---
+++ bai4.js
@@ -0,0 +1,12 @@
+//dem tu a den b
+let a, b
+function count( a, b ){
+ if ( a >= b ){
+   return 'you must enter number A < B'
+ }
+ else { for( a; a <= b; a++ ){
+    console.log(a)
+        } 
+    }
+}
+count(2,7)
